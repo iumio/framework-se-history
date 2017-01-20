e4fa28506b5f4f6c91a59fa9548e2fc5b25d50e5
RAFINA DANY <danyrafina@gmail.com>
2017-01-20 07:44:34 +0100

    Alpha 0.0.5

--- vendor/iumio_framework/php/Core/Requirement/IumioUltimaCore.php
+++ vendor/iumio_framework/php/Core/Requirement/IumioUltimaCore.php
@@ -31,8 +31,8 @@ abstract class IumioUltimaCore
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '0.0.4';
-    const VERSION_ID = 20814;
+    const VERSION = '0.0.5';
+    const VERSION_ID = 20815;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 0;
     const RELEASE_VERSION = 1;
