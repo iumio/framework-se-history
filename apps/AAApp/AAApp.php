bf8a4a693bdfa12e70f4140926d4d9f762683b58
RAFINA DANY <danyrafina@gmail.com>
2017-05-19 11:12:13 +0200

    Fix app component directory with adding index.php

---
+++ apps/AAApp/AAApp.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AAApp;
+
+use iumioFramework\Core\Requirement\iumioAppModel as App;
+
+/**
+ * Class AAApp
+ * @package AAApp
+ */
+
+class AAApp extends App
+{
+    /*** REGISTER TO APP ***/
+
+}
