8097335b579892e83290d9a93e1eb59c2a882d97
RAFINA DANY <danyrafina@gmail.com>
2017-06-13 08:43:26 +0200

    Change version

--- vendor/iumio_framework/php/Core/Requirement/iumioUltimaCore.php
+++ vendor/iumio_framework/php/Core/Requirement/iumioUltimaCore.php
@@ -37,11 +37,11 @@ abstract class iumioUltimaCore extends iumioUltima
     protected $environment;
     private static $runtime_parameters = null;
 
-    const VERSION = '0.2.0';
+    const VERSION = '0.2.2';
     const VERSION_EDITION = 'iumio Framework Standard Edition';
     const VERSION_EDITION_SHORT = 'SE';
     const VERSION_STAGE = 'BETA';
-    const VERSION_BUILD = 201720;
+    const VERSION_BUILD = 201722;
 
 
 
